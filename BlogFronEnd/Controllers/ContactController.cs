7c18de47d9803d5b3af5a6ef31c67f4c57f9e315
kobiewolke <kobiewolke@gmail.com>
2022-12-18 00:34:26 +0200

    Blog System Check in

---
+++ BlogFronEnd/Controllers/ContactController.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace BlogFronEnd.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Blog.DTO.ContactDTO contact)
+        {
+            postContact(contact);                        
+            return Redirect("/Blog");
+        }
+
+        private void postContact(Blog.DTO.ContactDTO contact)
+        {
+            string Response = "";
+            string url = ConfigurationManager.AppSettings["APIUrl"].ToString();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage HttpResponseMessage = null;
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            var myContent = jss.Serialize(contact);
+
+            var httpContent = new StringContent(myContent, Encoding.UTF8, "application/json");            
+            HttpResponseMessage = client.PostAsync("SaveContact", httpContent).Result;
+
+            if (HttpResponseMessage.StatusCode == HttpStatusCode.OK)
+            {
+                Response = HttpResponseMessage.Content.ReadAsStringAsync().Result;
+            }            
+        }
+    }
+}
---
+++ BlogTest/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BlogTest.Controllers
+{
+    public class ContactController : ApiController
+    {
+        [HttpPost]
+        [Route("api/SaveContact")]
+        public IHttpActionResult SaveContact(Blog.DTO.ContactDTO contact)
+        {
+            try
+            {
+                Blog.DAL.ContactDAL contactDAL = new Blog.DAL.ContactDAL(); 
+                Blog.DTO.ContactDTO contactDTO = new Blog.DTO.ContactDTO();                
+                
+                return Ok(contactDAL.SaveContact(contact));
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { message = ex.Message});
+            }
+        }
+    }
+}
